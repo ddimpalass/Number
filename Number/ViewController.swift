5b576ae81745782595e344952a9f017a3ca60410
Дмитрий Щелочков <ddimpalass@icloud.com>
2020-12-14 09:52:10 +0300

    Initial Commit

---
+++ Number/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Number
+//
+//  Created by Apple on 14.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
